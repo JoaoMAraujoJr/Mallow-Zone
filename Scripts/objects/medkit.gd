4694ca26a0848fc730733b6f54869bf9e1d57d9b
João Araujo <juniormedeirosales@gmail.com>
2025-12-27 18:10:01 -0300

    added medkit

--- Scripts/objects/medkit.gd
+++ Scripts/objects/medkit.gd
@@ -1,7 +1,23 @@
 extends Node2D
-
+@onready var CaseAnimPlayer : AnimationPlayer = $SubViewportContainer/SubViewport/Node3D/AnimationPlayer
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PlayerArea"):
 		body.addToHealth(25)
 		queue_free()
+
+
+func _on_player_detector_body_entered(body: Node2D) -> void:
+	if body is Player:
+		CaseAnimPlayer.play("OpenCase")
+		await CaseAnimPlayer.animation_finished
+		CaseAnimPlayer.play("CaseOpened")
+	pass # Replace with function body.
+
+
+func _on_player_detector_body_exited(body: Node2D) -> void:
+	if body is Player:
+		CaseAnimPlayer.play_backwards("OpenCase")
+		await CaseAnimPlayer.animation_finished
+		CaseAnimPlayer.play("CaseClosed")
+	pass # Replace with function body.
