d20121df852d0072a5a9297d41b9dfda282db2b8
João Araujo <juniormedeirosales@gmail.com>
2026-04-04 19:17:50 -0300

    minor cleanup
    just backing up some things

--- Scripts/Resources/room_entry.gd
+++ Scripts/Resources/room_entry.gd
@@ -3,3 +3,8 @@ class_name room_entry
 
 @export_file("*.tscn") var room_path : String
 @export_range(1,1000) var weight : int = 10
+
+@export var north : bool
+@export var south : bool
+@export var east : bool
+@export var west : bool
--- Scripts/Unique/procedurally_generated_place.gd
+++ Scripts/Unique/procedurally_generated_place.gd
@@ -2,3 +2,4 @@ extends Node2D
 class_name ProceduralPlaceRoot
 
 var taken_chunks : Array[Vector2i] = []
+var grid := {} # Dictionary<Vector2i, Array[RoomEntry]]
